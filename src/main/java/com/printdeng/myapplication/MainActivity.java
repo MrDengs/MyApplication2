fed39714c228eabb943e0b314ff6a71fee3cdb82
MrsDengs <printlndeng.@sina.com>
2017-11-25 16:50:12 +0800

    你好

--- src/main/java/com/printdeng/myapplication/MainActivity.java
+++ src/main/java/com/printdeng/myapplication/MainActivity.java
@@ -3,12 +3,14 @@ package com.printdeng.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public abstract class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
+    public abstract void get();
+
 }
+
